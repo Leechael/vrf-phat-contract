823b663e2ad413193a01222ae3350a2d5043b7c9
Leechael Yim <yanleech@gmail.com>
2023-11-06 18:17:36 +0800

    format: indent

--- foundry_solidity/src/RandomPokemon.sol
+++ foundry_solidity/src/RandomPokemon.sol
@@ -8,52 +8,52 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@phala/solidity/contracts/PhatRollupAnchor.sol";
 
 contract RandomPokemon is ERC721, Ownable, PhatRollupAnchor {
-    using Strings for uint256;
+  using Strings for uint256;
 
-    event ResponseReceived(uint reqId, string reqData, bytes value);
-    event ErrorReceived(uint reqId, string reqData, uint256 errno);
-    event PokemonEncountered(uint reqId, uint256 id, uint256 level, uint256 hp, uint256 attack, uint256 defense, uint256 specialAttack, uint256 specialDefense, uint256 speed);
+  event ResponseReceived(uint reqId, string reqData, bytes value);
+  event ErrorReceived(uint reqId, string reqData, uint256 errno);
+  event PokemonEncountered(uint reqId, uint256 id, uint256 level, uint256 hp, uint256 attack, uint256 defense, uint256 specialAttack, uint256 specialDefense, uint256 speed);
 
-    uint constant TYPE_RESPONSE = 0;
-    uint constant TYPE_ERROR = 2;
+  uint constant TYPE_RESPONSE = 0;
+  uint constant TYPE_ERROR = 2;
 
-    mapping(uint => string) requests;
-    uint nextTokenId = 1;
+  mapping(uint => string) requests;
+  uint nextTokenId = 1;
 
-    string public emptyTokenURI;
-    string public baseTokenURI;
+  string public emptyTokenURI;
+  string public baseTokenURI;
 
-    constructor(address phatAttestor) ERC721("", "") PhatRollupAnchor()  {
-        _grantRole(PhatRollupAnchor.ATTESTOR_ROLE, phatAttestor);
+  constructor(address phatAttestor) ERC721("", "") PhatRollupAnchor()  {
+    _grantRole(PhatRollupAnchor.ATTESTOR_ROLE, phatAttestor);
+  }
+
+  //
+  // PhatRollupAnchor
+  //
+
+  function setAttestor(address phatAttestor) public {
+    _grantRole(PhatRollupAnchor.ATTESTOR_ROLE, phatAttestor);
+  }
+
+  function _onMessageReceived(bytes calldata action) internal override {
+    // Optional to check length of action
+    // require(action.length == 32 * 3, "cannot parse action");
+    (uint respType, uint reqId, bytes memory data) = abi.decode(
+      action,
+      (uint, uint, bytes)
+    );
+    if (respType == TYPE_RESPONSE) {
+      emit ResponseReceived(reqId, requests[reqId], data);
+      delete requests[reqId];
+      uint256[] memory stats = abi.decode(data, (uint256[]));
+      require(stats.length == 8, "stats should 8 numbers");
+      emit PokemonEncountered(reqId, stats[0], stats[1], stats[2], stats[3], stats[4], stats[5], stats[6], stats[7]);
+    } else if (respType == TYPE_ERROR) {
+      uint errcode = abi.decode(data, (uint));
+      emit ErrorReceived(reqId, requests[reqId], errcode);
+      delete requests[reqId];
     }
-
-    //
-    // PhatRollupAnchor
-    //
-
-    function setAttestor(address phatAttestor) public {
-        _grantRole(PhatRollupAnchor.ATTESTOR_ROLE, phatAttestor);
-    }
-
-    function _onMessageReceived(bytes calldata action) internal override {
-        // Optional to check length of action
-        // require(action.length == 32 * 3, "cannot parse action");
-        (uint respType, uint reqId, bytes memory data) = abi.decode(
-            action,
-            (uint, uint, bytes)
-        );
-        if (respType == TYPE_RESPONSE) {
-            emit ResponseReceived(reqId, requests[reqId], data);
-            delete requests[reqId];
-            uint256[] memory stats = abi.decode(data, (uint256[]));
-            require(stats.length == 8, "stats should 8 numbers");
-            emit PokemonEncountered(reqId, stats[0], stats[1], stats[2], stats[3], stats[4], stats[5], stats[6], stats[7]);
-        } else if (respType == TYPE_ERROR) {
-            uint errcode = abi.decode(data, (uint));
-            emit ErrorReceived(reqId, requests[reqId], errcode);
-            delete requests[reqId];
-        }
-    }
+  }
 
     //
     // ERC721
@@ -71,12 +71,12 @@ contract RandomPokemon is ERC721, Ownable, PhatRollupAnchor {
   }
 
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-      _requireMinted(tokenId);
-      if (keccak256(abi.encodePacked(requests[tokenId])) != keccak256(abi.encodePacked(""))) {
-        return bytes(emptyTokenURI).length > 0 ? emptyTokenURI : "";
-      }
-      string memory baseURI = _baseURI();
-      return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+    _requireMinted(tokenId);
+    if (keccak256(abi.encodePacked(requests[tokenId])) != keccak256(abi.encodePacked(""))) {
+      return bytes(emptyTokenURI).length > 0 ? emptyTokenURI : "";
+    }
+    string memory baseURI = _baseURI();
+    return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
   }
 
   function _baseURI() internal view virtual override returns (string memory) {
@@ -89,9 +89,9 @@ contract RandomPokemon is ERC721, Ownable, PhatRollupAnchor {
   }
 
   function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC721) returns (bool) {
-      return
-          interfaceId == type(IERC721).interfaceId ||
-          interfaceId == type(IERC721Metadata).interfaceId ||
-          super.supportsInterface(interfaceId);
+    return
+      interfaceId == type(IERC721).interfaceId ||
+      interfaceId == type(IERC721Metadata).interfaceId ||
+      super.supportsInterface(interfaceId);
   }
 }
